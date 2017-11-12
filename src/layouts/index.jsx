3130adc95f4c3bf4011e9f71957036b29dc19f5c
PoiScript <poiscript@gmail.com>
2017-11-12 21:41:17 +0800

    refactor: clean up border styling

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,21 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
 import 'katex/dist/katex.min.css'
 
-import { BottomBorder, TopBorder } from './border'
 import { LeftTop, RightBottom } from './carve'
+import Border from '../assets/border.png'
 
 import './index.css'
 
+const Container = styled.div`
+  z-index: 99;
+  margin: 15px 0;
+  padding: 15px 0;
+  border: 15px solid transparent;
+  border-image: url(${Border}) 60 round;
+`
+
 const App = ({ children }) => (
-  <div>
+  <Container>
     <LeftTop />
-    <TopBorder />
     {children()}
-    <BottomBorder />
     <RightBottom />
-  </div>
+  </Container>
 )
 
 App.propTypes = {
